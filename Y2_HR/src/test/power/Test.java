074f02e1ade11e23850499a6c21c9627aad0d338
BarryLiu <1025587160@qq.com>
2015-11-23 20:57:59 +0800

    ..

--- Y2_HR/src/test/power/Test.java
+++ Y2_HR/src/test/power/Test.java
@@ -22,6 +22,7 @@ public class Test {
 		testselectRightForRoleId();
 	}
 
+	//
 	private static void testselectRightForRoleId() {
 		RightMapper mapper = (RightMapper) cxt.getBean("rightMapper");
 		
